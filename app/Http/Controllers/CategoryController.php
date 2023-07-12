8a8e39d7f4cb35c309c34311a411f33417954429
Vincenzo <vincenzor.celu@gmail.com>
2023-07-12 15:03:00 -0400

    Acomodando controlador category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $categories = Category::all();
@@ -17,17 +15,11 @@ class CategoryController extends Controller
         return view('categories.index', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -43,9 +35,6 @@ class CategoryController extends Controller
         return redirect()->route('categories.index')->with('success','Nueva categoria agregada');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         $category = Category::find($id);
@@ -53,17 +42,11 @@ class CategoryController extends Controller
         return view('categories.show', compact('category'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $category = Category::find($id);
@@ -73,10 +56,7 @@ class CategoryController extends Controller
 
         return redirect()->route('categories.index')->with('success','categoria actualizada');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
         $category = Category::find($id);
